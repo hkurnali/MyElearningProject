c16d6825b4839e1ae699825a1ac5fbd3869b2f28
huseyin kurnalı <huseyinkurnali@hotmail.com>
2023-09-19 18:54:29 +0300

    Add project files.

---
+++ MyElearningProject/DAL/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyElearningProject.DAL.Entities
+{
+    public class Category
+
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+    }
+}
